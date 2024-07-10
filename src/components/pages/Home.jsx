eb5d8fcceaf727ebb327eb2ec99c1b4be2f2b4cc
torihazi <lovebarusa19981225@icloud.com>
2024-07-10 15:35:48 +0900

    fin helloworld

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { Box, Typography } from "@mui/material";
 
 const Home = () => {
-  return <div>Home</div>;
+  const words = "HELLO_TORIHAZI";
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="calc(100vh - 64px)"
+    >
+      <Typography variant="h1" color="primary" fontWeight="bold">
+        {words.split("").map((key, value) => (
+          <motion.span
+            key={value}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.5,
+              delay: value * 0.1,
+              repeat: Infinity,
+            }}
+          >
+            {key}
+          </motion.span>
+        ))}
+      </Typography>
+    </Box>
+  );
 };
 
 export default Home;
